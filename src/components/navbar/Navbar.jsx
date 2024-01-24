a7c7e5fcd6ae0ee6b266207beaeed53569ff016e
Adityaa404 <aditya.sharma@primathon.in>
2024-01-24 12:50:03 +0530

    logo click redirect added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,15 @@
 import "./navbar.css"
+import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navContainer">
-        <span className="logo">
-          JetSetLOGO
-          {/* <image src={JetSetLogo} alt="JetSetGO Logo"
-          ></image> */}
-        </span>
+      <Link to="/" style={{ textDecoration: 'none' }}>
+          <span className="logo">
+            JetSetLOGO
+            {/* <image src={JetSetLogo} alt="JetSetGO Logo" /> */}
+          </span>
+        </Link>
         <div className="navItems">
           <button className="navButton">Register</button>
           <button className="navButton">Login</button>
